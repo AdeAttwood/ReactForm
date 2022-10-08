39c38e528f29cea2056d19defb107de41a76aa08
Ade Attwood <adrian.attwood@marketdojo.com>
2022-10-08 11:42:45 +0100

    feat: add radio input

---
+++ src/attribute-context.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export type AttributeContextType<T> = {
+  /**
+   * The name of this attribute that the context if for
+   */
+  attribute: string;
+  /**
+   * A list of options that can be passed with the attribute if this type
+   * requests multiple options
+   */
+  options: T[];
+}
+
+/**
+ * Create the base context that will hold all of the form data
+ */
+export const AttributeContext = React.createContext<AttributeContextType<any>>({} as any);
+
+/**
+ * Hook so that you can access the form data
+ */
+export function useAttributeContext<T extends {}>(): AttributeContextType<T> {
+  return React.useContext(AttributeContext);
+}
---
+++ tests/radio-group.spec.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Form from "../src/form";
+import { RadioGroup, RadioOption } from "../src/radio-group";
+
+const options = [
+  { label: "One", value: "one" },
+  { label: "Two", value: "two" },
+];
+
+it("will render and submit with a radio list", async () => {
+  const onSubmit = jest.fn();
+  const { getByLabelText, getByText } = render(
+    <Form initialValues={{}} onSubmit={onSubmit}>
+      <RadioGroup attribute="pickOne" options={options}>
+        {() => (
+          <RadioOption>
+            {({ props, label }) => (
+              <div key={props.id} className="form-check">
+                <input className="form-check-input" {...props} />
+                <label className="form-check-label" htmlFor={props.id}>
+                  {label}
+                </label>
+              </div>
+            )}
+          </RadioOption>
+        )}
+      </RadioGroup>
+      <button>Submit</button>
+    </Form>
+  );
+
+  await userEvent.click(getByLabelText("One"));
+  await userEvent.click(getByText("Submit"));
+
+  expect(onSubmit).toBeCalledTimes(1);
+  expect(onSubmit).toBeCalledWith({ formState: { pickOne: 'one' } });
+
+  await userEvent.click(getByLabelText("Two"));
+  await userEvent.click(getByText("Submit"));
+
+  expect(onSubmit).toBeCalledTimes(2);
+  expect(onSubmit).toBeCalledWith({ formState: { pickOne: 'two' } });
+});
