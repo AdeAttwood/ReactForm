c193f46dddbd04100c143cdac1e1840ac4fdf9bd
George Cadwallader <george@practically.io>
2022-11-18 21:17:34 +0000

    feat: create more tests and add documentation for check group component
    Adds some extra tests for the check group input component, making sure
    some extra conditions are covered. Primarily testing submitting a
    for with a check input when it has a default value, and making sure that
    the form does not submit when the validation for the check input
    attribute does not pass.
    
    Also added some documentation for this component, creating a page and
    providing an example.
    
    Ref: #37

--- tests/check-group.spec.tsx
+++ tests/check-group.spec.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Form from "../src/form";
 import CheckGroup from "../src/check-group";
+import createValidator from "../src/validator";
 
 it("will render and submit with a checkbox attribute", async () => {
   const onSubmit = jest.fn();
@@ -31,4 +32,64 @@ it("will render and submit with a checkbox attribute", async () => {
 
   expect(onSubmit).toBeCalledTimes(2);
   expect(onSubmit).toBeCalledWith(expect.objectContaining({ formState: { checkMe: false } }));
+});
+
+it("will have a default value and submit", async () => {
+  const onSubmit = jest.fn();
+  const { getByText } = render(
+    <Form initialValues={{ input: true }} onSubmit={onSubmit}>
+      <CheckGroup attribute="input">
+        {({ props }) => (
+          <div>
+            <label htmlFor={props.id}>Check</label>
+            <input {...props} />
+          </div>
+        )}
+      </CheckGroup>
+      <button>Submit</button>
+    </Form>
+  );
+
+  await userEvent.click(getByText("Submit"));
+  await waitFor(() => expect(onSubmit).toBeCalledTimes(1));
+  expect(onSubmit).toBeCalledWith(
+    expect.objectContaining({
+      formState: { input: true },
+    })
+  );
+});
+
+it("will validate on save and prevent submit", async () => {
+  const onSubmit = jest.fn();
+  const { getByText } = render(
+    <Form
+      onSubmit={onSubmit}
+      initialValues={{ input: false }}
+      validator={createValidator({
+        input: [
+          ({ input }) => {
+            if (!input) {
+              return "Input cannot be false";
+            }
+          },
+        ],
+      })}
+    >
+      <CheckGroup attribute="input">
+        {({ props }) => (
+          <div>
+            <label htmlFor={props.id}>Check</label>
+            <input {...props} />
+          </div>
+        )}
+      </CheckGroup>
+      <button>submit</button>
+    </Form>
+  );
+
+  await act(async () => {
+    fireEvent.click(getByText("submit"));
+  });
+
+  expect(onSubmit).toBeCalledTimes(0);
 });
