a745f3a931fc313fa781ab61c98fd902cce4fc3b
Ade Attwood <adrian.attwood@marketdojo.com>
2023-01-28 08:03:05 +0000

    feat: allow errors to be set from the onSubmit callback
    Now when an error is thrown from the `onSubmit` callback if the object
    contains an `errorBag` property the contents of that property will be
    interpreted as an "ErrorBag" and set as the errors. The status will also
    be updated with the correct status for the errors being set.
    
    ```js
    const onSubmit = () => {
      throw { errorBag: { attributeOne: ["List of error messages"] } };
    }
    ```
    
    This is useful for when server side validation fails and you want to
    update the UI for the user.

--- tests/on-submit.spec.tsx
+++ tests/on-submit.spec.tsx
@@ -4,6 +4,7 @@ import userEvent from "@testing-library/user-event";
 
 import Form, { OnSubmitFunction } from "../src/form";
 import Input from "./input-component";
+import { useFormContext } from "../src/form-context";
 
 afterEach(cleanup);
 
@@ -25,4 +26,50 @@ it("will call onSubmit", async () => {
   });
 
   expect(onSubmitMock).toHaveBeenCalled();
+});
+
+it("will set the errors from the onSubmit callback", async () => {
+  const onSubmit: OnSubmitFunction = () => {
+    throw {
+      errorBag: { "test-input": ["This is a error from the errors"] },
+    };
+  };
+
+  render(
+    <Form onSubmit={onSubmit}>
+      <Input attribute="test-input" />
+      <button>Submit</button>
+    </Form>
+  );
+
+  await act(async () => {
+    await userEvent.click(screen.getByText("Submit"));
+  });
+
+  expect(screen.getByText("This is a error from the errors"));
+});
+
+it("will set the status to clean if the errors are cleared", async () => {
+  const onSubmit: OnSubmitFunction = () => {
+    throw { errorBag: {} };
+  };
+
+  function StatusComponent() {
+    const { status } = useFormContext();
+    return <span>Status: {status}</span>;
+  }
+
+  render(
+    <Form onSubmit={onSubmit}>
+      <Input attribute="test-input" />
+      <StatusComponent />
+      <button>Submit</button>
+    </Form>
+  );
+
+  await act(async () => {
+    await userEvent.click(screen.getByText("Submit"));
+  });
+
+  expect(screen.getByText("Status: clean"));
 });
