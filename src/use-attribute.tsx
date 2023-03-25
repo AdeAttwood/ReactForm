4ba4e4237f62c27926a3378708992fcf6652a442
Ade Attwood <adrian.attwood@marketdojo.com>
2023-03-25 09:44:08 +0000

    feat: add better generic support for useAttribute hook
    Ref: #152

--- src/use-attribute.tsx
+++ src/use-attribute.tsx
@@ -7,13 +7,13 @@ export function createUseAttributeHook<T>(convert: (value: any) => T) {
     return {
       id: attribute,
       error: firstError(attribute),
-      value: getAttribute(attribute, defaultValue || ""),
+      value: getAttribute(attribute, defaultValue || "") as T,
       set: (value: any) => setAttribute(attribute, convert(value)),
     };
   };
 }
 
-export const useAttribute = createUseAttributeHook<string>((value) => value);
+export const useAttribute = createUseAttributeHook((value) => value);
 export const useStringAttribute = createUseAttributeHook<string>((value) => String(value));
 export const useDateAttribute = createUseAttributeHook<Date>((value) => new Date(value));
 export const useBooleanAttribute = createUseAttributeHook<boolean>((value) => Boolean(value));
--- tests/use-attribute.spec.tsx
+++ tests/use-attribute.spec.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { renderHook, act } from "@testing-library/react";
+import { renderHook, act, cleanup } from "@testing-library/react";
 
 import { Form } from "../src/form";
 import { useAttribute, useBooleanAttribute, useStringAttribute } from "../src/use-attribute";
+
+afterEach(cleanup);
 
 const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => {
   return <Form onSubmit={jest.fn}>{children}</Form>;
@@ -14,6 +16,29 @@ it("useAttribute will mount and update the base attribute hook", () => {
   expect(result.current.value).toBe("");
   act(() => result.current.set("Hello"));
   expect(result.current.value).toBe("Hello");
+});
+
+it("useAttribute can be used as a generic and the value is of the same type", () => {
+  type User = { firstName: string };
+  const user = { firstName: "John" };
+  const { result } = renderHook(() => useAttribute("testing1", user), { wrapper });
+
+  function getFirstName(user: User) {
+    return user.firstName;
+  }
+
+  expect(getFirstName(result.current.value)).toBe("John");
+});
+
+test.each([
+  [1, 1],
+  [null, null],
+  ["testing", "testing"],
+  [{ one: "two" }, { one: "two" }],
+])("useAttribute %s is cast to a anything and will be %s", (value, expected) => {
+  const { result } = renderHook(() => useAttribute("testing", ""), { wrapper });
+  act(() => result.current.set(value));
+  expect(result.current.value).toStrictEqual(expected);
 });
 
 test.each([
