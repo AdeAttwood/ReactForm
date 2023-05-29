11fce9edf0b99d5038a1993cd3ae7040ffe3dd24
Ade Attwood <code@adeattwood.co.uk>
2023-05-29 20:12:01 +0100

    chore: update the react prism v2 new api

--- docs/components/code.tsx
+++ docs/components/code.tsx
@@ -1,16 +1,15 @@
-import Highlight, { defaultProps, Language } from "prism-react-renderer";
-import theme from "prism-react-renderer/themes/github";
+import { Highlight, themes } from "prism-react-renderer";
 import React from "react";
 
 function Code({ children, className }: any) {
-  const language = className?.replace(/language-/, "") as Language;
+  const language = className?.replace(/language-/, "");
 
   if (!language) {
     return <code>{children}</code>;
   }
 
   return (
-    <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language}>
+    <Highlight theme={themes.github} code={children.trim()} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className + " px-3 py-4"} style={{ ...style }}>
           {tokens.map((line, i) => (
